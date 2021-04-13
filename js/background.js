4302ae1d9b7b7a346ce0e0532eb06c2fc9131019
Desmond Ip <ipkinghangdesmond@gmail.com>
2021-04-13 21:42:56 +0800

    WIP

--- js/background.js
+++ js/background.js
@@ -1,6 +1,7 @@
+console.log("background running");
 const page_data = {};
+let all_tabs = [];
 let current_tab_id = 0;
-
 
 apiFetch = () =>{
     fetch(`http://www.omdbapi.com/?s=matrix&apikey=adf1f2d7`)
@@ -9,7 +10,6 @@ apiFetch = () =>{
       console.log("api fetch response:", data);
     });
 }
-
 
 apiPost = (outbound_data) => {
   fetch("https://httpbin.org/post", {
@@ -22,78 +22,43 @@ apiPost = (outbound_data) => {
   });
 }
 
-console.log("background running");
-chrome.tabs.onActivated.addListener(tab => {
-  console.log(tab); // gets the tab id and window id of tab
-  chrome.tabs.get(tab.tabId, current_tab_info => {    //pass the id into get to get the info
-    // console.log(current_tab_info);
-    console.log(current_tab_info);
-    console.log("tab.tabId", tab.tabId);
-    current_tab_id = tab.tabId;
 
-    page_data.url = current_tab_info.url;
-    page_data.title = current_tab_info.title;
-    chrome.tabs.executeScript(null, {file: "js/foreground.js"}, () => {
-      console.log("setting data.");
-      chrome.storage.local.set({"page_data": page_data.title});
+chrome.tabs.query({currentWindow: true}, currentTabs => {
+// data returns an array of current open tabs. URL in tab object
+  console.log("current tabs", currentTabs);
 
 
-      chrome.tabs.sendMessage(current_tab_id, {message: "check the storage"});
-      console.log("executing script");
-    });
-
-
-
-    apiFetch();
-    console.log("posting");
-    apiPost(page_data);
-
-
-  });
 });
 
 
-//get tab information
-chrome.tabs.query({active: false}, function(tab) {
-  // console.log(tab);
-  // chrome.tabs.sendMessage(tab.id, { }, function(response) {
-  //   console.log(response);
-  // });
+chrome.tabs.onCreated.addListener(newTab => {
+  console.log("new tab:", newTab);
+
+});
+
+chrome.tabs.onUpdated.addListener(updatedTab => {
+  console.log("updated tab:", updatedTab);
+
 });
 
 
+// chrome.tabs.onUpdated.addListener(tab => {
+//   console.log(tab); // gets the tab id and window id of tab
+//   chrome.tabs.get(tab.tabId, current_tab_info => {    //pass the id into get to get the info
+//     // console.log(current_tab_info);
+//     console.log(current_tab_info);
+//     console.log("tab.tabId", tab.tabId);
+//     current_tab_id = tab.tabId;
 
+//     page_data.url = current_tab_info.url;
+//     page_data.title = current_tab_info.title;
+//     chrome.tabs.executeScript(null, {file: "js/foreground.js"}, () => {
+//       console.log("executing script");
+//       console.log("setting data.");
+//       chrome.storage.local.set({"page_data": page_data.title});
 
-
-//aggy's code
-// console.log('hello from first extension')
-// var contextMenus = {};
-// contextMenus.sayHello =
-//     chrome.contextMenus.create(
-//         {
-//           "id": "sayHelloMenu",
-//           "title":"Say Hello 👋"
-//         },
-//         function (){
-//             if(chrome.runtime.lastError){
-//                 console.error(chrome.runtime.lastError.message);
-//             }
-//         }
-//     );
-// chrome.contextMenus.onClicked.addListener(contextMenuHandler);
-// function contextMenuHandler(info, tab){
-//     console.log("info: ", info)
-//     console.log("tab: ", tab)
-//     if(info.menuItemId===contextMenus.sayHello){
-//       // chrome.scripting.executeScript({
-//       //   code: `window.prompt("Hello 👋");`
-//       // })
-//       // const tabId = getTabId();
-//       const tabURL = tab.url;
-
-//       console.log("got the url", tabURL);
-//       chrome.tabs.executeScript({
-//             file: 'foreground.js'
-//           });
-//     }
-// }
+//       console.log("sending message")
+//       chrome.tabs.sendMessage(current_tab_id, {message: "check the storage"});
+//     });
+//   });
+// });
--- js/popup.js
+++ js/popup.js
@@ -1,10 +1,29 @@
 // popup.js
 console.log("popup.js running");
+// document.addEventListener('DOMContentLoaded', function() {
+//   var checkButton = document.getElementById('check');
+//   checkButton.addEventListener('click', function() {
+//    console.log("btn is working");
+//    alert("Hey your button is working!");
+
+//   }, false);
+// }, false);
+
 document.addEventListener('DOMContentLoaded', function() {
   var checkButton = document.getElementById('check');
   checkButton.addEventListener('click', function() {
-   console.log("btn is working");
-   alert("Hey your button is working!");
+
+    chrome.tabs.query({currentWindow: true}, currentTabs => {
+    // data returns an array of current open tabs. URL in tab object
+    console.log("current tabs", currentTabs);
+
+
+});
+
+
+
+
+
 
   }, false);
 }, false);
