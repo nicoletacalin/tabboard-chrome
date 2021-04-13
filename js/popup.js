9a0142d7c569c6db0d9418b24b3b4a7b008d5241
Desmond Ip <ipkinghangdesmond@gmail.com>
2021-04-13 11:27:13 +0800

    created manifest and added JS files

---
+++ js/popup.js
@@ -0,0 +1,7 @@
+// popup.js
+document.addEventListener('DOMContentLoaded', function() {
+  var checkButton = document.getElementById('check');
+  checkButton.addEventListener('click', function() {
+   alert("Hey your button is working!");
+  }, false);
+}, false);
