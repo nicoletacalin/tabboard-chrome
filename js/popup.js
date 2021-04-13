4153bb4362ef6cfa4bb9c387fdb2e7d589b27657
Desmond Ip <ipkinghangdesmond@gmail.com>
2021-04-13 20:32:33 +0800

    Post and Get working, communication between chrome frond and back end working. ( : Unicorns : )

--- js/popup.js
+++ js/popup.js
@@ -1,7 +1,10 @@
 // popup.js
+console.log("popup.js running");
 document.addEventListener('DOMContentLoaded', function() {
   var checkButton = document.getElementById('check');
   checkButton.addEventListener('click', function() {
+   console.log("btn is working");
    alert("Hey your button is working!");
+
   }, false);
 }, false);
