8e3f5eb950e4a8cb4b1dad9ed0eb981524fa4c70
XiwayBanh <xiway.banh@gmail.com>
2021-04-19 18:36:29 +0800

    updated sign out

---
+++ js/popup-signed-in-script.js
@@ -0,0 +1,12 @@
+document.querySelector('#sign-out').addEventListener('click', function () {
+    chrome.runtime.sendMessage({ message: 'logout' }, function (response) {
+        if (response === 'success') window.close();
+    });
+});
+
+document.querySelector('#test-btn').addEventListener('click', function () {
+  console.log('in test btn')
+    chrome.runtime.sendMessage({ message: 'isUserSignedIn' }, function (response) {
+        alert(response);
+    });
+});
