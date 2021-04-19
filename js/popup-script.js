684a60410a3591d9df43b8e0e0dc4b3a42671f2d
XiwayBanh <xiway.banh@gmail.com>
2021-04-19 17:52:44 +0800

    created sign in function and working on sign out

--- js/popup-script.js
+++ js/popup-script.js
@@ -1,4 +1,5 @@
 document.querySelector('#sign-in').addEventListener('click', function () {
+  console.log('in sign in')
     chrome.runtime.sendMessage({ message: 'login' }, function (response) {
         if (response === 'success') window.close();
     });
