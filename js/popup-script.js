5b555a324868149146b7d81245c243d28c582d57
Kevin Kurczseitz <kevinkurczseitz@icloud.com>
2021-04-28 20:32:17 +0800

    implemented the sign in function so that it opens and switches to the webapp when logging in the chrome extension

--- js/popup-script.js
+++ js/popup-script.js
@@ -3,6 +3,12 @@ document.querySelector('#sign-in').addEventListener('click', function () {
     chrome.runtime.sendMessage({ message: 'login' }, function (response) {
         if (response === 'success') window.close();
     });
+    // comment: opens a new tab (the webapp) upon sign in on extension for good ux
+    var action_url = "https://taboard.herokuapp.com/users/sign_in";
+    setTimeout(() => {
+      chrome.tabs.create({ url: action_url });
+    }, 2000);
+
 });
 // document.querySelector('#test-btn').addEventListener('click', function () {
 //     console.log('in test btn')
